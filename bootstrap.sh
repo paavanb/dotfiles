d5329c438329b6d013be8b3acd0f2624774f8978
Paavan Bhavsar <pbhavsar@kyru.us>
2013-08-20 16:40:38 -0400

    Added exclude_files.txt to make bootstrapping cleaner. Excludes .git and .gitignore files in all directories, not just root.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 git pull
 function updateHomeDir() {
-	rsync --exclude ".git/" --exclude ".gitignore" --exclude ".gitmodules" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rsync --exclude-from './exclude_files.txt' -av . ~
     # install C extension for vim's Command-T plugin
     cd ~/.vim/bundle/command-t/plugin; rake make; cd -
 }
